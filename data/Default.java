e3f532976f16cae121e1501bfb45a20c2f2f904e
leigh966 <leigh966@hotmail.com>
2022-12-19 14:02:08 +0000

    implemented a basic app, currently standing in for a plugin until plugin loading is complete, that renders a basic square room defined in level1.modat

--- data/Default.java
+++ data/Default.java
@@ -1,4 +1,16 @@
 package data;
 
+import data.libs.Map;
+
+import javax.swing.*;
+
 public class Default {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Map("level1").setVisible(true);
+            }
+        });
+    }
 }
