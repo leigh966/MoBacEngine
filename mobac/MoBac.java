74f11a9036f2109237289c933a9eef5a342dd1fc
leigh966 <leigh966@hotmail.com>
2022-12-19 15:10:31 +0000

    Increased modularisation and error handling in mobac.Mobac

--- mobac/MoBac.java
+++ mobac/MoBac.java
@@ -4,19 +4,31 @@ import mobac.MyClassLoader;
 
 public class MoBac
 {
+    private static ClassLoader parentClassLoader;
+    private static MyClassLoader classLoader;
+
+    private static void loadPlugin(String pluginName) throws IllegalAccessException, InstantiationException
+    {
+        try {
+            Class myObjectClass = classLoader.loadClass("data."+pluginName);
+            IMoBacPlugin object1 =
+                    (IMoBacPlugin) myObjectClass.newInstance();
+
+            object1.Run();
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.out.println("Plugin not found. Skipping...\nSee below for more information:");
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws
-            ClassNotFoundException,
             IllegalAccessException,
             InstantiationException {
 
-        ClassLoader parentClassLoader = MyClassLoader.class.getClassLoader();
-        MyClassLoader classLoader = new MyClassLoader(parentClassLoader);
-        Class myObjectClass = classLoader.loadClass("data.Default");
-
-        IMoBacPlugin object1 =
-                (IMoBacPlugin) myObjectClass.newInstance();
-
-        object1.Run();
-
+        parentClassLoader = MyClassLoader.class.getClassLoader();
+        classLoader = new MyClassLoader(parentClassLoader);
+        loadPlugin("Default");
     }
 }
