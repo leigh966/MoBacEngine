408daf4a81ab41a85832d0319f6bd5e1a23e5568
leigh966 <leigh966@hotmail.com>
2022-12-20 11:51:27 +0000

    added more error checking and modularisation

--- mobac/MoBac.java
+++ mobac/MoBac.java
@@ -7,7 +7,7 @@ public class MoBac
     private static ClassLoader parentClassLoader;
     private static MyClassLoader classLoader;
 
-    private static void loadPlugin(String pluginName) throws IllegalAccessException, InstantiationException
+    private static boolean loadPlugin(String pluginName) throws IllegalAccessException, InstantiationException
     {
         try {
             Class myObjectClass = classLoader.loadClass("data."+pluginName);
@@ -15,20 +15,36 @@ public class MoBac
                     (IMoBacPlugin) myObjectClass.newInstance();
 
             object1.Run();
+            return true;
         }
         catch(ClassNotFoundException e)
         {
             System.out.println("Plugin not found. Skipping...\nSee below for more information:");
             e.printStackTrace();
+            return false;
         }
     }
 
-    public static void main(String[] args) throws
-            IllegalAccessException,
-            InstantiationException {
+    private static boolean tryLoadPlugin(String name)
+    {
+        try
+        {
+            boolean loaded = loadPlugin(name);
+            return loaded;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong when loading plugin. Attempting to skip it...");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
 
         parentClassLoader = MyClassLoader.class.getClassLoader();
         classLoader = new MyClassLoader(parentClassLoader);
-        loadPlugin("Default");
+        tryLoadPlugin("Default");
+
     }
 }
