bbb0cf99d2520eba09dbf7f94c740c49d6e1a7b0
leigh966 <leigh966@hotmail.com>
2022-12-22 17:42:49 +0000

    Made player radius a static puplic global so that users can set any playerRadius that they want

--- data/libs/CollisionResolution.java
+++ data/libs/CollisionResolution.java
@@ -5,15 +5,15 @@ import data.libs.abstract_classes.Render;
 import java.util.List;
 
 public class CollisionResolution {
+    public static float playerRadius = 1f;
     public static void PlayerLine(Render r)
     {
         Player player = r.getPlayer();
         List<Float[]> lines = r.getLines();
-        final float PLAYER_RADIUS = 2f;
         float[] playerPos = player.getPosition();
         for(Float[] line : lines)
         {
-            Collision2D col = Collision2D.CircleLine(playerPos[0], playerPos[1], PLAYER_RADIUS,
+            Collision2D col = Collision2D.CircleLine(playerPos[0], playerPos[1], playerRadius,
                     line[0], line[1], line[0]+line[2], line[0]+line[3]);
             if(col.getCollisionOccurred())
             {
@@ -21,7 +21,7 @@ public class CollisionResolution {
                 float[] toPlayer = new float[] {playerPos[0]-intersection[0], playerPos[1]-intersection[1]};
                 float currentDistance = MoMath.pythagorasTheorem(toPlayer[0], toPlayer[1]);
                 float[] normalised = MoMath.normalize(toPlayer[0], toPlayer[1], currentDistance);
-                float deltaDistance = PLAYER_RADIUS-currentDistance;
+                float deltaDistance = playerRadius -currentDistance;
                 player.transform(normalised[0]*deltaDistance, normalised[1]*deltaDistance);
             }
         }
