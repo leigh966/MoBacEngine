17b8f7bf82423307679dc38e6e8cbd97df7223e5
leigh966 <leigh966@hotmail.com>
2022-12-25 01:04:46 +0000

    Documented ConfigReader with Java doc comments

--- src/main/java/data/libs/ConfigReader.java
+++ src/main/java/data/libs/ConfigReader.java
@@ -5,8 +5,15 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * A class for reading in and interpreting values from config files
+ */
 public class ConfigReader {
-
+    /**
+     * Read in a config with the given name and return a hashmap of its contained values
+     * @param name The name of the config to read (e.g. rendering or controls)
+     * @return Hashmap of the key-value pairs contained in the config
+     */
     public static HashMap<String, String> readConfig(String name)
     {
         HashMap<String, String> configValues = new HashMap<>();
